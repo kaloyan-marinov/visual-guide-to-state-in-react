907c11a6fa9a539cb42a3e5c2b6e2e47f2eec81a
Kaloyan Marinov <kallo.marinov@gmail.com>
2020-09-12 09:31:05 +0300

    add 1 CSS class

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 
 class App extends React.Component {
   render() {
-    return <div>Hello world!</div>;
+    return <div className="container">Hello world!</div>;
   }
 }
 
